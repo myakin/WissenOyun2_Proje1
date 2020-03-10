dd6c80fc3f78221247e0d968aedad18362530137
Murat Yakin <yakin_murat@yahoo.com>
2020-03-10 03:20:20 +0300

    Bilardo masasi fizigine giris

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float magnitude;
+    public GameObject cue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(cue.transform.up*magnitude);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/TurnAroundBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnAroundBall : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    private float rotationAmount;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotationAmount -= rotationSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotationAmount += rotationSpeed;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, rotationAmount);
+        
+        
+    }
+}
